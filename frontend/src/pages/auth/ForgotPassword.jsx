25dd552bc47f6e520a7c9287598ddf02580ed82f
kuldeepkarmaa <144059420+kuldeepkarmaa@users.noreply.github.com>
2026-06-22 14:15:07 +0530

    Implement Forgot Password component

--- frontend/src/pages/auth/ForgotPassword.jsx
+++ frontend/src/pages/auth/ForgotPassword.jsx
@@ -1,1 +1,48 @@
+import React from "react";
+import {Mail} from "lucide-react";
+import {Link} from "react-router-dom";
+import "../../App.css";
 
+const ForgotPassword =()=>{
+return(
+<div className="auth-page">
+<div className="login-card">
+<div className="auth-logo">
+<div className="logo-box">
+🧠
+</div>
+<h2>
+Forgot Password?
+</h2>
+<p>
+Enter your email to reset password
+</p>
+</div>
+
+<div className="input-group">
+<label>
+Email Address
+</label>
+<div className="input-box">
+<Mail size={18}/>
+<input
+type="email"
+placeholder="name@company.com"
+/>
+</div>
+</div>
+<button className="login-btn">
+Send Reset Link
+</button>
+<p className="register-text">
+Remember password?
+<Link to="/login">
+Login
+</Link>
+</p>
+</div>
+</div>
+)
+}
+
+export default ForgotPassword;
