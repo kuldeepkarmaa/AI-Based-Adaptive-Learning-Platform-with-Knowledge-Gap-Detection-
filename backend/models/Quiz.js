2ae5cc7c46eaaddc92af0fe7c91b3e952ff8d311
kuldeepkarmaa <kuldeepkarma816@gmail.com>
2026-06-28 01:47:02 +0530

    Update Quiz.js
    Changes

--- backend/models/Quiz.js
+++ backend/models/Quiz.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const mongoose = require('mongoose');
 
 const QuizSchema = new mongoose.Schema({
@@ -43,3 +44,73 @@ const QuizSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model('Quiz', QuizSchema);
+=======
+const mongoose = require("mongoose");
+
+const QuestionSchema = new mongoose.Schema({
+  questionText: {
+    type: String,
+    required: true,
+  },
+
+  answerOptions: [
+    {
+      text: {
+        type: String,
+        required: true,
+      },
+
+      isCorrect: {
+        type: Boolean,
+        required: true,
+      },
+
+      rationale: String,
+    },
+  ],
+
+  hint: {
+    type: String,
+    required: true,
+  },
+
+  difficulty: {
+    type: String,
+    enum: ["Easy", "Medium", "Hard"],
+    default: "Medium",
+  },
+});
+
+const QuizSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    topic: {
+      type: String,
+      required: true,
+    },
+
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    questions: [QuestionSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Quiz", QuizSchema);
+>>>>>>> Stashed changes
