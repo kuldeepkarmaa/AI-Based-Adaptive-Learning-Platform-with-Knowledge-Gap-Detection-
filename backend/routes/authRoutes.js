26c67d412b07eacce01043cb8bd9e0340524e44b
kunalndrio <kunal.ndrio@gmail.com>
2026-07-21 15:18:13 +0530

    Modified Comflict

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser , googleLogin } = require('../controllers/authController');
+const { registerUser, loginUser, googleLogin, getMe } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
 
-// Mapping Endpoints
+// Public Endpoints
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/google', googleLogin);
+
+// Protected Endpoint
+router.get('/me', protect, getMe);
 
 module.exports = router;
